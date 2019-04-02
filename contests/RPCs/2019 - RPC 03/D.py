667045837b234fcaf255431bea57e9cbd622e50f
Juaanma <juanmanuelsuarez10@gmail.com>
2019-04-01 21:23:48 -0300

    D - 2019 - RPC 03

--- contests/RPCs/2019 - RPC 03/D.py
+++ contests/RPCs/2019 - RPC 03/D.py
@@ -1,3 +1,21 @@
+M = int(1e9+9)
 k, b = map(int, input().split())
 
-print ((2**b-1) // k) % (1e9 + 9)
+def pot(b, p):
+    return b**p%k
+
+def sub(a, b):
+    return ((a-b)%k+k)%k
+
+m = {}
+def f(p, r):
+    if p == -1:
+        return 0
+
+    if (p, r) not in m:
+        act = (2**(p+1)-1+k-r)//k - (2**p-1+k-r)//k
+        m[(p, r)] = (f(p-1, r) + f(p-1, sub(r, pot(2, p))) + act) % M 
+
+    return m[(p, r)]
+
+print(f(b-1, 0))
