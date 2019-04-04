04e77b302cf0008a4073129859bbecfa3704d985
NataTassi <natanieltassi@gmail.com>
2019-04-04 00:17:23 -0300

    combinatorics fixed

--- math/combinatorics.cpp
+++ math/combinatorics.cpp
@@ -1,4 +1,4 @@
-ll MAXS = 1e3+1, S[MAXS][MAXS];
+int MAXS = 1e3+1, S[MAXS][MAXS];
 void stirling(){
 	S[0][0] = 1;
 	forsn(i,1,N) S[i][0] = S[0][i] = 0;
@@ -6,13 +6,13 @@ void stirling(){
 		S[i][j] = add(mul(S[i-1][j],j),S[i-1][j-1]);
 }
 
-ll MAXC = 1e3+1, C[MAXC][MAXC];
-forn(i, MAXC + 1){ // C[n][k] = C(n, k)
+int MAXC = 1e3+1, C[MAXC][MAXC];
+forn(i,MAXC){ // C[n][k] = C(n, k)
     C[i][0] = C[i][i] = 1;
-    forsn(k, 1, i) C[i][k] = add(C[i-1][k] + C[i-1][k-1]);
+    forsn(k,1,i) C[i][k] = add(C[i-1][k],C[i-1][k-1]);
 }
 
-ll bell[MAXC+1][MAXC+1];
+int bell[MAXC+1][MAXC+1];
 ll bell(int n){
     bell[0] = 1;
     forsn(i,1,n+1) forsn(k,0,i)
