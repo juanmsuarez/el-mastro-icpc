8bc41a8f2dfe2caef3fa7fe6b62ac7bb6617c166
Juaanma <juanmanuelsuarez10@gmail.com>
2019-04-05 15:49:06 -0300

    Actualizo WF - 2019 - Juan - D

--- contests/WFs/2019/Juan/D.cpp
+++ contests/WFs/2019/Juan/D.cpp
@@ -1,3 +1,4 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -25,80 +26,37 @@ using vii = vector<pii>;
 using ll = long long;
 
 const int N = 1e6;
-vi ord, v[N], marks[N];
+vii v;
+int cnt, len[N], sum[N], sz[N];
 bool on[N];
-int n, ind[N];
 
-int aStart(int i) {
-    int m = si(v[i])/2, sum = 0;
-    forn(j, m) sum += v[i][j];
-    if (sum != 0) return -1;
+void advance(int i) {
+    int ind, t; tie(ind, t) = v[i];
 
-    int j = 0, k = 0;
-    while (j < m) {
-        sum = 0;
-        while (k < j + m && sum >= 0) sum += v[i][k++];
-        if (sum >= 0) return j;
-        j = k;
-    } 
-}
+    if (sum[ind] == 0 && len[ind] == sz[ind]) cnt--;
 
-void markStarts(int i, int s) {
-    int m = si(v[i])/2, sum = 0;
+    if (len[ind] != sz[ind]) len[ind]++;
+    sum[ind] += t;
+    if (sum[ind] < 0) sum[ind] = 0, len[ind] = 0;
 
-    marks[i] = vi(m);
-    forsn(j, s, s+m) {
-        marks[i][j%m] = sum == 0;
-        sum += v[i][j];
-    }
-}
-
-pii getMax() {
-    int sum = 0;
-    dforn(k, n) {
-        int i = ord[k], &j = ind[i], m = si(marks[i]);
-        if (marks[i].empty()) continue;
-        if (marks[i][m-1-j]) on[i] = true, sum++;
-        else if (on[i]) on[i] = false, sum--;
-        j = (j+1)%m;
-    }
-
-    pii ans = {0, 0};
-    dforn(k, n) {
-        int i = ord[k], &j = ind[i], m = si(marks[i]);
-
-        if (!marks[i].empty()) {
-            if (marks[i][m-1-j]) on[i] = true, sum++;
-            else if (on[i]) on[i] = false, sum--;
-            j = (j+1)%m; 
-        }
-
-        ans = max(ans, {sum, -k});
-    }
-
-    return ans;
+    if (sum[ind] == 0 && len[ind] == sz[ind]) cnt++;
 }
 
 int main() {
 	fastio;
 	
+    int n;
     cin >> n;
     forn(i, n) {
         char c; int t; cin >> c >> t;
-        v[t-1].pb(c == 's' ? 1 : -1);
-        ord.pb(t-1);
+        v.pb(t-1, c == 's' ? 1 : -1);
+        sz[t-1]++;
     }
 
-    forn(i, N)
-        if (!v[i].empty()) {
-            int m = si(v[i]);
-            forn(j, m) v[i].pb(v[i][j]);
+    forn(i, n) advance(i);
+    pii ans = {0, 0};
+    forn(i, n) ans = max(ans, {cnt, -i}), advance(i);
 
-            int s = aStart(i);
-            if (s != -1) markStarts(i, s);
-        }
-
-    pii ans = getMax();
     cout << -ans.snd+1 << ' ' << ans.fst << endl;
 	
     return 0;
