0323011dda62a66a5d34762dfa43be39bcd94e01
NataTassi <natanieltassi@gmail.com>
2019-05-04 18:20:44 -0300

    frac updated

--- math/frac.cpp
+++ math/frac.cpp
@@ -1,4 +1,5 @@
 template<class T> T gcd(T a,T b){return b==0?a:gcd(b,a%b);}
+
 struct frac{
 	int p,q;
 	frac(int p=0, int q=1):p(p),q(q) {norm();}
@@ -8,16 +9,17 @@ struct frac{
 		if(q < 0) q=-q, p=-p;}
 	frac operator+(const frac& o){
 		int a = gcd(q,o.q);
-		return frac(p*(o.q/a) + o.p*(q/a), q*(o.q/a));}
+		return frac(add(mul(p,o.q/a), mul(o.p,q/a)), mul(q,o.q/a));}
 	frac operator-(const frac& o){
 		int a = gcd(q,o.q);
-		return frac(p*(o.q/a) - o.p*(q/a), q*(o.q/a));}
+		return frac(sub(mul(p,o.q/a), mul(o.p,q/a)), mul(q,o.q/a));}
 	frac operator*(frac o){
 		int a = gcd(q,o.p), b = gcd(o.q,p);
-		return frac((p/b)*(o.p/a), (q/a)*(o.q/b));}
+		return frac(mul(p/b,o.p/a), mul(q/a,o.q/b));}
 	frac operator/(frac o){
 		int a = gcd(q,o.q), b = gcd(o.p,p);
-		return frac((p/b)*(o.q/a), (q/a)*(o.p/b));}
+		return frac(mul(p/b,o.q/a), mul(q/a,o.p/b));}
 	bool operator<(const frac &o) const{return ll(p)*o.q < ll(o.p)*q;}
 	bool operator==(frac o){return p==o.p && q==o.q;}
+	bool operator!=(frac o){return p!=o.p || q!=o.q;}
 };
