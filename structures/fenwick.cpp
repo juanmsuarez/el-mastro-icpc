df71998a12aafaa0cc8aa4cbd5c492fa88c94aaa
NataTassi <natanieltassi@gmail.com>
2019-05-03 22:17:01 -0300

    Fenwick updated

--- structures/fenwick.cpp
+++ structures/fenwick.cpp
@@ -1,8 +1,8 @@
-typedef ll tipo;
 // Para 2D: tratar cada columna como un Fenwick Tree, 
 // agregando un for anidado en cada operacion.
 // Trucazo para 2D: si los elementos no se repiten,
 // se puede usar un ordered set para memoria O(n*log^2(n))
+typedef ll tipo;
 struct Fenwick {
     static const int sz = (1 << 18) + 1;
     tipo t[sz];
@@ -15,15 +15,10 @@ struct Fenwick {
         return s;
     }
     tipo sum(int a, int b){ return sum(b) - sum(a - 1); }
-    // Obtiene el menor valor con suma acumulada >= x
-    // Para mayor, llamar a getind(v + 1) - 1, creo
-    int getind(tipo v) {
-        int x = 0;
-        for(int d = 1 << 18; d; d >>= 1)
-            if(t[x|d] < v){
-                x |= d;
-                v -= t[x];
-            }
-        return x + 1; 
+    int lower_bound(tipo v) { // Menor x con suma acumulada >= v, O(lg n)
+        int x = 0, d = sz-1;
+        if(v > t[d]) return sz;
+        for(; d; d >>= 1) if(t[x|d] < v) v -= t[x |= d];
+        return x+1; 
     }
 };
