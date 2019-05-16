8428cbabd80672b542e33702468c02e2ea2572ab
Juaanma <juanmanuelsuarez10@gmail.com>
2019-05-15 21:58:00 -0300

    Agrego control en Union Find

--- structures/union.find.cpp
+++ structures/union.find.cpp
@@ -1,15 +1,18 @@
-struct DSU { // Operations take O(log*(n))
+struct UF { // Operations take O(log*(n))
     vi p,s;
-    DSU(int n){ p.resize(n), iota(all(p),0), s.assign(n,1); }
-    int root(int i){
-	    while(p[i] != i) p[i] = p[p[i]], i = p[i];
+    UF(int n){ p.resize(n), iota(all(p), 0), s.assign(n, 1); }
+    int find(int i){
+	    while (p[i] != i) p[i] = p[p[i]], i = p[i];
 	    return i;
     }
-    bool find(int x, int y){ return root(x) == root(y); }
-    void join(int x, int y){
-    	x = root(x), y = root(y);
-    	if(s[x] < s[y]) p[x] = y, s[y] += s[x];
+    bool con(int x, int y){ return find(x) == find(y); }
+    bool join(int x, int y){
+    	x = find(x), y = find(y);
+        if (con(x, y)) return false;
+
+    	if (s[x] < s[y]) p[x] = y, s[y] += s[x];
     	else p[y] = x, s[x] += s[y];
+        return true;
     }
 };
 
