5f3314e2ccc88d8a4f0b9cad996e8a4377e0b922
Juaanma <juanmanuelsuarez10@gmail.com>
2019-07-10 00:39:53 -0300

    Contest 1 - Problema H

--- contests/TC 2019/Contest 1/E.cpp
+++ contests/TC 2019/Contest 1/E.cpp
@@ -1,34 +1,61 @@
 #include <bits/stdc++.h>
-#define forsn(i,s,n) for(int i = (int) s; i < (int)n;i++)
-#define forn(i,n) forsn(i,0,n)
 using namespace std;
 
+#ifdef LOCAL
+	#define D(a) cerr << #a << " = " << a << endl
+#else
+	#define D(a)
+	#define cerr false && cerr
+#endif
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0)
+#define dforsn(i,s,n) for(int i=int(n-1);i>=int(s);i--)
+#define forsn(i,s,n) for(int i=int(s);i<int(n);i++)
+#define dforn(i,n) dforsn(i,0,n)
+#define forn(i,n) forsn(i,0,n)
+#define all(a) a.begin(),a.end()
+#define si(a) int((a).size())
 #define pb emplace_back
-#define si(v) (int)((v).size())
+#define mp make_pair
+#define snd second
+#define fst first
+#define endl '\n'
+using pii = pair<int,int>;
 using vi = vector<int>;
+using ll = long long;
 
-int main(){
-    int n, k; cin >> n >> k;
+const int N = 1e5, M = 1e9+7;
+int dp[N+1];
+
+int add(int a, int b) {
+    return a + b >= M ? a + b - M : a + b;
+}
+
+int main() {
+	fastio;
+	
+    int n; cin >> n;
     vi a(n); forn(i, n) cin >> a[i];
 
-    vector<vi> ans(n);
-    while (k > 0) {
-        bool dif = false;
-        while (!dif) {
-            forn(i, n) 
-                if (a[i] > 0) a[i]--, ans[i].pb(k);
-                else dif = true;
+    dp[0] = 1;
+    forn(i, n) {
+        int num = a[i];
+
+        vi divs;
+        for (int d = 1; d * d <= num; d++) if (num % d == 0) {
+            divs.pb(d);
+            if (d * d != num) divs.pb(num / d);
         }
-        k--;
-    } 
+        sort(all(divs));
 
-    bool can = true;
-    forn(i, n) if (a[i] != 0) can = false;
-    if (can) {
-        cout << "YES" << endl;
-        for (vi &v : ans) forn(i, si(v)) cout << v[i] << " \n"[i==si(v)-1];
+        dforn(j, si(divs)) {
+            int d = divs[j];
+            if (d <= N) dp[d] = add(dp[d], dp[d-1]);
+        }
     }
-    else cout << "NO" << endl;
 
-    return 0;
+    int ans = 0;
+    forsn(i, 1, n+1) ans = add(ans, dp[i]);
+    cout << ans << endl;
+	
+	return 0;
 }
