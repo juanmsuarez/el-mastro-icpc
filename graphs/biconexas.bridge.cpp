c983c730de42aad8674336bba83fc14c77acdb17
NataTassi <natanieltassi@gmail.com>
2019-07-15 21:52:03 -0300

    Actualizo biconexas

--- graphs/biconexas.bridge.cpp
+++ graphs/biconexas.bridge.cpp
@@ -1,26 +1,25 @@
 struct bridge {
-
 	struct edge {
-		int u,v, comp;
+		int u,v,comp;
 		bool bridge;
 	};
 
-	int n;
-	vi *adj;
+	int n,t,nbc;
+	vi d,b,comp;
+	stack<int> st;
+    vector<vi> adj;
 	vector<edge> e;
 
-	bridge(int n): n(n) {
-		adj = new vi[n];
+	bridge(int n=0): n(n) {
+		adj = vector<vi>(n);
 		e.clear();
 		initDfs();
 	}
 
 	void initDfs() {
-		d = new int[n];
-		b = new int[n];
-		comp = new int[n];
-		nbc = t = 0;
-        forn(u, n) d[u] = -1;
+        d = vi(n), b = vi(n), comp = vi(n);
+        forn(i,n) d[i] = -1;
+        nbc = t = 0;
 	}
 
 	void addEdge(int u, int v) {
@@ -28,36 +27,30 @@ struct bridge {
 		e.pb((edge){u,v,-1,false});
 	}
 
-
     //d[i]=id de la dfs
     //b[i]=lowest id reachable from i
-	int *d, *b, t, nbc;
-	int *comp;
-	stack<int> st;
 	void dfs(int u=0, int pe=-1) {
 		b[u] = d[u] = t++;
-		comp[u] = (pe != -1);
+        comp[u] = pe != -1;
 
-		forn(i,si(adj[u])) {
-			int ne = adj[u][i];
-			if (ne == pe) continue;
+		for(int ne : adj[u]) {
+			if(ne == pe) continue; 
 			int v = e[ne].u ^ e[ne].v ^ u;
-			if (d[v] == -1) {
+			if(d[v] == -1) {
 				st.push(ne);
 				dfs(v,ne);
-				if (b[v] > d[u]) e[ne].bridge = true; // bridge
-				if (b[v] >= d[u]) { // art
+				if(b[v] > d[u]) e[ne].bridge = true; // bridge
+				if(b[v] >= d[u]) { // art
 					int last;
 					do {
 						last = st.top(); st.pop();
 						e[last].comp = nbc;
-					} while (last != ne);
-					nbc++;
-					comp[u]++;
+					} while(last != ne);
+					nbc++, comp[u]++;
 				} 
 				b[u] = min(b[u], b[v]);
 			}
-			else if (d[v] < d[u]) { // back edge
+			else if(d[v] < d[u]) { // back edge
 				st.push(ne);
 				b[u] = min(b[u], d[v]);
 			}
