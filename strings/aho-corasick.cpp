d5d1e998f38055fc4e468d00c7f481ba188b36d6
Juaanma <juanmanuelsuarez10@gmail.com>
2019-09-01 13:21:07 -0300

    Actualizo Aho-Corasick

---
+++ strings/aho-corasick.cpp
@@ -0,0 +1,59 @@
+const int K = 26;
+
+// si el alfabeto es muy grande, adaptar usando map para next y go
+// es posible almacenar los indices de las palabras en terminal usando vector<int>
+struct Vertex {
+    int next[K];
+    int terminal = 0;
+    int p = -1;
+    char pch;
+    int link = -1;
+    int go[K];
+
+    Vertex(int p=-1, char ch='$') : p(p), pch(ch) {
+        fill(begin(next), end(next), -1);
+        fill(begin(go), end(go), -1);
+    }
+};
+
+vector<Vertex> t;
+
+void aho_init() { // INICIALIZAR!
+    t.clear(); t.pb(Vertex());
+}
+
+void add_string(string const& s) {
+    int v = 0;
+    for (char ch : s) {
+        int c = ch - 'a';
+        if (t[v].next[c] == -1) {
+            t[v].next[c] = si(t);
+            t.pb(v, ch);
+        }
+        v = t[v].next[c];
+    }
+    t[v].terminal++;
+}
+
+int go(int v, char ch);
+
+int get_link(int v) {
+    if (t[v].link == -1) {
+        if (v == 0 || t[v].p == 0)
+            t[v].link = 0;
+        else
+            t[v].link = go(get_link(t[v].p), t[v].pch);
+    }
+    return t[v].link;
+}
+
+int go(int v, char ch) {
+    int c = ch - 'a';
+    if (t[v].go[c] == -1) {
+        if (t[v].next[c] != -1)
+            t[v].go[c] = t[v].next[c];
+        else
+            t[v].go[c] = v == 0 ? 0 : go(get_link(v), ch);
+    }
+    return t[v].go[c];
+} 
--- structures/mod.cpp
+++ structures/mod.cpp
@@ -5,15 +5,15 @@ struct num {
     num operator +(num b){ return a+b.a >= M ? a+b.a-M : a+b.a; }
     num operator -(num b){ return a-b.a < 0 ? a-b.a+M : a-b.a; }
     num operator *(num b){ return int((ll)a*b.a % M); } 
-    num operator ^(num e){
-		if(!e.a) return 1;
-        num q = (*this)^num(e.a/2);
-		return e.a & 1 ? q*q*(*this) : q*q; 
+    num operator ^(ll e){
+		if(!e) return 1;
+        num q = (*this)^(e/2);
+		return e & 1 ? q*q*(*this) : q*q; 
     }
     num operator ++(int x){ return a++; }
 };
 int norm(ll x){ return x < 0 ? int(x % M + M) : int(x % M); }
-num inv(num x){ return x^num(M-2); } // M must be prime
+num inv(num x){ return x^(M-2); } // M must be prime
 num operator /(num a, num b){ return a*inv(b); } 
 num neg(num x){ return x.a ? -x.a+M : 0; }
 istream& operator >>(istream &i, num &x){ i >> x.a; return i; }
