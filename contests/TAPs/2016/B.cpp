3b4c1915ec81a615e3ac5bcb6f420471906b7a80
Juaanma <juanmanuelsuarez10@gmail.com>
2019-09-22 22:00:19 -0300

    TAP 2016 - B

--- contests/TAPs/2016/B.cpp
+++ contests/TAPs/2016/B.cpp
@@ -23,10 +23,58 @@ using pii = pair<int,int>;
 using vi = vector<int>;
 using ll = long long;
 
+struct Point {
+    ll x, y;
+
+    Point(ll _x = 0, ll _y = 0) : x(_x), y(_y) {}
+
+    Point operator - (Point o) { return Point(x - o.x, y - o.y); }
+    double norm() { return sqrt(x * x + y * y); }
+};
+
+double dist(Point p, Point q) {
+    return (p - q).norm();
+}
+
+using Poly = vector<Point>;
+
+const int N = 4e3, INF = 1e9;
+
+int n, target;
+Poly poly;
+
+int next(int x) { return (x + 1) % n; }
+int prev(int x) { return (x - 1 + n) % n; }
+
+double memo[N][N][2];
+double dp(int left, int right, bool last_left) {
+    int last = last_left ? left : right;
+    if (next(left) == right) return last == target ? 0 : INF;
+
+    double &res = memo[left][right][last_left];
+    if (res != -1) return res;
+
+    int next_left = next(left); 
+    double res_move_left = dp(next_left, right, true) 
+        + dist(poly[last], poly[next_left]);
+
+    int prev_right = prev(right); 
+    double res_move_right = dp(left, prev_right, false) 
+        + dist(poly[last], poly[prev_right]);
+
+    return res = min(res_move_left, res_move_right);
+}
+
 int main() {
 	fastio;
-	
 
+    fill(memo[0][0], memo[N][0], -1);
+    
+    int start; cin >> n >> start >> target; start--, target--;
+    poly.resize(n); for (Point &p : poly) cin >> p.x >> p.y;
+
+    double ans = dp(start, start, true);
+    cout << fixed << setprecision(6) << ans << endl;
 	
 	return 0;
 }
