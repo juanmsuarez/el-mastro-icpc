4d5b970f357018f31f180fb865b70bf76b1d45c7
NataTassi <natanieltassi@gmail.com>
2019-10-07 20:02:07 -0300

    minor tweak

--- geometry/orden.radial.cpp
+++ geometry/orden.radial.cpp
@@ -2,12 +2,12 @@
 // hacer operadores ^ y - constantes
 struct RadialOrder {
 	pto r;
-	RadialOrder(pto r) : r(r) {}
+	RadialOrder(pto _r) : r(_r) {}
 	int cuad(const pto &a) const {
-		if(a.x > 0 && a.y >= 0)return 0;
-		if(a.x <= 0 && a.y > 0)return 1;
-		if(a.x < 0 && a.y <= 0)return 2;
-		if(a.x >= 0 && a.y < 0)return 3;
+		if(a.x > 0 && a.y >= 0) return 0;
+		if(a.x <= 0 && a.y > 0) return 1;
+		if(a.x < 0 && a.y <= 0) return 2;
+		if(a.x >= 0 && a.y < 0) return 3;
 		return -1;
 	}
 	bool comp(const pto &p1, const pto &p2) const {
@@ -15,7 +15,7 @@ struct RadialOrder {
 		if (c1 == c2) return (p1 ^ p2) > 0;
         else return c1 < c2;
 	}
-    bool operator()(const pto&p1, const pto&p2) const {
+    bool operator()(const pto &p1, const pto &p2) const {
         return comp(p1 - r, p2 - r);
     }
 };
