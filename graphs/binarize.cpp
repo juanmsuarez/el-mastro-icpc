e714f860df98ac46945cc9dc095033842d98a0d6
Juaanma <juanmanuelsuarez10@gmail.com>
2019-10-22 14:48:07 -0300

    Actualizo binarize

--- graphs/binarize.cpp
+++ graphs/binarize.cpp
@@ -1,7 +1,11 @@
 const int N = 5e4, M = 2*N;
-int n;
+int m;
 vi adj[N]; 
 vii adjb[M];
+
+void init_binarization(int n) {
+    m = n;
+}
 
 void binarize(int u = 0, int p = -1) {
     int act = u;
@@ -9,7 +13,7 @@ void binarize(int u = 0, int p = -1) {
         adjb[act].pb(v, 1); adjb[v].pb(act, 1);
         binarize(v, u);
 
-        int w = n++; 
+        int w = m++; 
         adjb[act].pb(w, 0); adjb[w].pb(act, 0);
 
         act = w;
