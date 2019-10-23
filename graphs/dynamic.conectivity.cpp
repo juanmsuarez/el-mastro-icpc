69f7496be6026c707691f53586712a07d102ae96
Juaanma <juanmanuelsuarez10@gmail.com>
2019-10-23 16:02:59 -0300

    Actualizo Dynamic Connectivity y Union-Find

--- graphs/dynamic.conectivity.cpp
+++ graphs/dynamic.conectivity.cpp
@@ -1,105 +1,67 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define dprint(v) cerr << #v"=" << v << endl //;)
-#define forr(i,a,b) for(int i=(a); i<(b); i++)
-#define forn(i,n) forr(i,0,n)
-#define dforn(i,n) for(int i=n-1; i>=0; i--)
-#define forall(it,v) for(auto it=v.begin();it!=v.end();++it)
-#define sz(c) ((int)c.size())
-#define zero(v) memset(v, 0, sizeof(v))
-#define pb push_back
-#define fst first
-#define snd second
-#define mkp make_pair
-typedef long long ll;
-typedef pair<int,int> ii;
-
-struct UnionFind {
+struct UF {
     int n, comp;
-    vector<int> pre,si,c;
-    UnionFind(int n=0):n(n), comp(n), pre(n), si(n, 1) {
-        forn(i,n) pre[i] = i; }
-    int find(int u){return u==pre[u]?u:find(pre[u]);}
+    vi par, size, c;
+    UF(int n = 0): n(n), comp(n), par(n), size(n, 1) { iota(all(par), 0); }
+    int find(int u) { return u == par[u] ? u : find(par[u]); }
+    bool connected(int u, int v) { return find(u) == find(v); }
     bool merge(int u, int v) {
-        if((u=find(u))==(v=find(v))) return false;        
-        if(si[u]<si[v]) swap(u, v);
-        si[u]+=si[v], pre[v]=u, comp--, c.pb(v);
+        if (connected(u, v)) return false;        
+        u = find(u), v = find(v);
+
+        if (size[u] < size[v]) swap(u, v);
+        size[u] += size[v], par[v] = u, comp--, c.pb(v);
         return true;
     }
-    int snap(){return sz(c);}
+    int snap() { return si(c); }
     void rollback(int snap){
-        while(sz(c)>snap){
+        while (si(c) > snap) {
             int v = c.back(); c.pop_back();
-            si[pre[v]] -= si[v], pre[v] = v, comp++;
+            size[par[v]] -= size[v], par[v] = v, comp++;
         }
     }
 };
-enum {ADD,DEL,QUERY};
-struct Query {int type,u,v;};
+enum { ADD, DEL, QUERY };
+struct Query { int type, u, v; };
 struct DynCon {
     vector<Query> q;
-    UnionFind dsu;
-    vector<int> match,res;
-    map<ii,int> last;//se puede no usar cuando hay identificador para cada arista (mejora poco)
-    DynCon(int n=0):dsu(n){}
+    UF uf;
+    vi match; // match[i] = remove j asociado al add i (y viceversa)
+    map<pii, int> last; // last[{u, v}] = i tal que add i agrega {u, v}
+    vi res;
+    DynCon(int n=0): uf(n) {}
     void add(int u, int v) {
-        if(u>v) swap(u,v);
-        q.pb((Query){ADD, u, v}), match.pb(-1);
-        last[ii(u,v)] = sz(q)-1;
+        if (u > v) swap(u, v);
+        q.pb((Query){ADD, u, v}), match.pb(-1), last[{u, v}] = si(q) - 1;
     }
     void remove(int u, int v) {
-        if(u>v) swap(u,v);
+        if (u > v) swap(u, v);
         q.pb((Query){DEL, u, v});
-        int prev = last[ii(u,v)];
-        match[prev] = sz(q)-1;
-        match.pb(prev);
+        int prev = last[{u, v}]; match[prev] = si(q) - 1; match.pb(prev);
     }
-    void query() {//podria pasarle un puntero donde guardar la respuesta
-        q.pb((Query){QUERY, -1, -1}), match.pb(-1);}
-    void process() {
-        forn(i,sz(q)) if (q[i].type == ADD && match[i] == -1) match[i] = sz(q);
-        go(0,sz(q));
+    void query() {
+        q.pb((Query){QUERY, -1, -1}), match.pb(-1);
     }
-    void go(int l, int r) {
-        if(l+1==r){
-            if (q[l].type == QUERY)//Aqui responder la query usando el dsu!
-                res.pb(dsu.comp);//aqui query=cantidad de componentes conexas
+    void process() { // answers all queries in order
+        if (q.empty()) return;
+        forn(i, si(q)) if (q[i].type == ADD && match[i] == -1) match[i] = si(q);
+        go(0, si(q));
+    }
+    void go(int l, int r) { // divide intervalo al medio y procesa por partes, O(k log k)
+        if (l+1 == r) {
+            if (q[l].type == QUERY) // answer query using UF 
+                res.pb(uf.comp); // aqui query=cantidad de componentes conexas
             return;
         }
-        int s=dsu.snap(), m = (l+r) / 2;
-        forr(i,m,r) if(match[i]!=-1 && match[i]<l) dsu.merge(q[i].u, q[i].v);
-        go(l,m);
-        dsu.rollback(s);
-        s = dsu.snap();
-        forr(i,l,m) if(match[i]!=-1 && match[i]>=r) dsu.merge(q[i].u, q[i].v);
-        go(m,r);
-        dsu.rollback(s);
+        int m = (l+r) / 2;
+
+        int s = uf.snap(); 
+        dforsn(i, m, r) if (match[i] != -1 && match[i] < l) uf.merge(q[i].u, q[i].v);
+        go(l, m); uf.rollback(s);
+
+        s = uf.snap();
+        dforsn(i, l, m) if (match[i] != -1 && match[i] >= r) uf.merge(q[i].u, q[i].v);
+        go(m, r); uf.rollback(s);
     }
-}dc;
+};
 
-// Problema ejemplo: http://codeforces.com/gym/100551/problem/A
-
-int n,k;
-
-int main() {
-    //~ freopen("in", "r", stdin);
-    freopen("connect.in", "r", stdin);
-    freopen("connect.out", "w", stdout);
-    ios::sync_with_stdio(0);
-    while(cin >> n >> k){
-		dc=DynCon(n);
-		forn(_,k) { string ord; cin >> ord;
-			if (ord=="?") {
-				dc.query();
-			} else if (ord=="+") { int a,b; cin>>a>>b; a--;b--;
-				dc.add(a,b);
-			} else if (ord=="-") { int a,b; cin>>a>>b; a--;b--;
-				dc.remove(a,b);
-			} else assert(false);
-		}
-        if(!k) continue;//k==0 WTF
-        dc.process();
-        forn(i,sz(dc.res)) cout << dc.res[i] << endl;
-    }
-    return 0;
-}
+// Primero agregar queries, adds y removes, luego llamar a process
--- structures/union.find.cpp
+++ structures/union.find.cpp
@@ -1,14 +1,14 @@
 struct UF {
-    vi par, si;
-    UF(int n) { par.resize(n), iota(all(par), 0), si.assign(n, 1); }
+    vi par, size;
+    UF(int n): par(n), size(n, 1) { iota(all(par), 0); }
     int find(int u) { return par[u] == u ? u : par[u] = find(par[u]); }
     bool connected(int u, int v) { return find(u) == find(v); }
     bool join(int u, int v) {
-    	u = find(u), v = find(v);
         if (connected(u, v)) return false;
+        u = find(u), v = find(v);
 
-    	if (si[u] < si[v]) par[u] = v, si[v] += si[u];
-    	else par[v] = u, si[u] += si[v];
+        if (size[u] < size[v]) par[u] = v, size[v] += size[u];
+        else par[v] = u, size[u] += size[v];
         return true;
     }
 };
