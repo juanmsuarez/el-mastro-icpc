cd7d9c6488e945bc7ed85031d18d40693f5f2f9a
Juaanma <juanmanuelsuarez10@gmail.com>
2019-10-31 22:21:01 -0300

    Agrego Edmonds Karp

--- flow/edmonds.karp.cpp
+++ flow/edmonds.karp.cpp
@@ -1,34 +1,47 @@
-#define MAX_V 1000
-#define INF 1e9
-//special nodes
-#define SRC 0
-#define SNK 1
-map<int, int> G[MAX_V];//limpiar esto
-//To add an edge use
-#define add(a, b, w) G[a][b]=w
-int f, p[MAX_V];
-void augment(int v, int minE){
-	if(v==SRC) f=minE;
-	else if(p[v]!=-1){
-		augment(p[v], min(minE, G[p[v]][v]));
-		G[p[v]][v]-=f, G[v][p[v]]+=f;
-	}
+int n;
+vvi capacity; // cuidado int!
+vvi adj;
+
+int bfs(int s, int t, vector<int>& parent) {
+    fill(parent.begin(), parent.end(), -1);
+    parent[s] = -2;
+    queue<pii> q;
+    q.push({s, INF});
+
+    while (!q.empty()) {
+        int cur = q.front().first;
+        int flow = q.front().second;
+        q.pop();
+
+        for (int next : adj[cur]) {
+            if (parent[next] == -1 && capacity[cur][next]) {
+                parent[next] = cur;
+                int new_flow = min(flow, capacity[cur][next]);
+                if (next == t)
+                    return new_flow;
+                q.push({next, new_flow});
+            }
+        }
+    }
+
+    return 0;
 }
-ll maxflow(){//O(VE^2)
-	ll Mf=0;
-	do{
-		f=0;
-		char used[MAX_V]; queue<int> q; q.push(SRC);
-		zero(used), memset(p, -1, sizeof(p));
-		while(sz(q)){
-			int u=q.front(); q.pop();
-			if(u==SNK) break;
-			forall(it, G[u])
-				if(it->snd>0 && !used[it->fst])
-					used[it->fst]=true, q.push(it->fst), p[it->fst]=u;
-		}
-		augment(SNK, INF);
-		Mf+=f;
-	}while(f);
-	return Mf;
+
+int maxflow(int s, int t) {
+    int flow = 0;
+    vi parent(n);
+    int new_flow;
+
+    while (new_flow = bfs(s, t, parent)) {
+        flow += new_flow;
+        int cur = t;
+        while (cur != s) {
+            int prev = parent[cur];
+            capacity[prev][cur] -= new_flow;
+            capacity[cur][prev] += new_flow;
+            cur = prev;
+        }
+    }
+
+    return flow;
 }
