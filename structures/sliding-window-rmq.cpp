362fdca459654540888347efed7c0f60f9664698
NataTassi <natanieltassi@gmail.com>
2019-10-31 22:34:09 -0300

    minor tweak

--- structures/sliding-window-rmq.cpp
+++ structures/sliding-window-rmq.cpp
@@ -4,21 +4,13 @@ struct RMQ {
     deque<T> d; queue<T> q;
     void push(T v) { 
         while (!d.empty() && Compare()(d.back(), v)) d.pop_back(); 
-        d.pb(v); 
-        q.push(v);
+        d.pb(v), q.push(v);
     }
-
     void pop() { 
         if (!d.empty() && d.front()==q.front()) d.pop_front(); 
         q.pop(); 
     }
-
-    T getMax() {
-        return d.empty() ? INF : d.front();
-    }
-
-    int size() {
-        return si(q);
-    }
+    T getMax() { return d.empty() ? INF : d.front(); }
+    int size() { return si(q); }
 };
 RMQ<ll, less<ll>, -INF> rmq;
