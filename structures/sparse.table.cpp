99c6ea66baad1a788817420730b682f9c0506b52
NataTassi <natanieltassi@gmail.com>
2019-11-11 20:06:35 -0300

    sparse table tweaks

--- structures/sparse.table.cpp
+++ structures/sparse.table.cpp
@@ -1,19 +1,16 @@
-// Dado un arreglo y una operacion asociativa idempotente:
-// get(i, j) opera sobre el rango [i, j).
-// Restriccion: 2^K > N. Usar [ ] para llenar
-// el arreglo y luego build().
+// The operation has to be associative and idempotent
+#define lg(n) 31 - __builtin_clz(n)
+typedef int T; 
 struct RMQ {
-    const static int K = ;
-    tipo vec[K][1 << K];
-    tipo &operator [](int p){ return vec[0][p]; }
-    tipo get(int i, int j){ // intervalo [i, j)
-        int p = 31 - __builtin_clz(j - i);
-        return min(vec[p][i], vec[p][j - (1 << p)]);
+    const static int K = 10; // 2^K > N
+    T t[K][1 << K];
+    T& operator[](int p){ return t[0][p]; }
+    T get(int i, int j){ // O(1), [i, j)
+        int p = lg(j-i);
+        return min(t[p][i], t[p][j - (1 << p)]);
     }
     void build(int n){ // O(n log n)
-        int mp = 31 - __builtin_clz(n);
-        forn(p, mp) 
-            forn(x, n - (1 << p))
-                vec[p + 1][x] = min(vec[p][x], vec[p][x + (1 << p)]);
+        forn(p, lg(n)) forn(x, n - (1 << p))
+            t[p + 1][x] = min(t[p][x], t[p][x + (1 << p)]);
     }
-};
+} rmq;
