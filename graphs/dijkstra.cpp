4f5dfa04a83630536931642da10e6e6031564fc2
NataTassi <natanieltassi@gmail.com>
2020-03-15 00:36:32 -0300

    manoseo dijkstra

--- graphs/dijkstra.cpp
+++ graphs/dijkstra.cpp
@@ -1,38 +1,28 @@
-vector<pii> adj[N]; // IMPORTANTE: ver tipo arco
-//To add an edge (u,v) with cost p use G[u].pb(v,p)
-ll dist[N];
-int dad[N];
-bool seen[N];
+vector<pii> adj[N];
+ll dist[N]; int par[N]; bool seen[N];
 
-ll dijkstra(int s=0, int t=-1) {//O(|E| log |V|)
-    fill(dist, dist+N, INF);
-    fill(dad, dad+N, -1);
-    fill(seen, seen+N, false);
+ll dijkstra(int n, int s=0, int t=-1) { // O(E lg V)
+    forn(i, n) dist[i] = INF, seen[i] = 0, par[i] = -1;
+	priority_queue<pii, vector<pii>, greater<pii>> q;
+	q.emplace(0, s), dist[s] = 0;
 
-	priority_queue<pii, vector<pii>, greater<pii>> pq;
-	pq.emplace(0, s); dist[s] = 0;
-
-	while (!pq.empty()){
-		int u = pq.top().snd; pq.pop();
-
+	while (!q.empty()){
+		int u = q.top().snd; q.pop();
         if (seen[u]) continue;
         seen[u] = true;
-
 		if (u == t) break;
-
-		for (auto e : adj[u]) {
-            int v, p; tie(v, p) = e;
-			if (dist[u] + p < dist[v]) {
-				dist[v] = dist[u] + p;
-				dad[v] = u;
-				pq.emplace(dist[v], v);	
+		for (auto &e : adj[u]) {
+            int v, w; tie(v, w) = e;
+			if (dist[u] + w < dist[v]) {
+				dist[v] = dist[u] + w;
+				par[v] = u;
+				q.emplace(dist[v], v);	
             }
         }
 	}
-
 	return t != -1 ? dist[t] : 0;
 }
 // path generator
-if (dist[t] < INF)
-    for (int u = t; u != -1; u = dad[u])
-        cout << u << " \n"[u == s];
+vi path;
+if (dist[t] < INF) 
+    for (int u = t; u != -1; u = par[u]) path.pb(u);
