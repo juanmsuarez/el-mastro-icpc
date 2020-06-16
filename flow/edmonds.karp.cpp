1970b613e1068962d99c3b7cf0b5f4a69767d21c
NataTassi <natanieltassi@gmail.com>
2020-06-16 13:36:15 -0300

    update EK

--- flow/edmonds.karp.cpp
+++ flow/edmonds.karp.cpp
@@ -1,46 +1,54 @@
-int n;
-vector<vi> adj;
-vector<vector<ll>> capacity;
+const int INF = 1e9;
+template<class T>
+struct EK {
+    int n;
+    vector<vi> adj;
+    vector<vector<T>> capacity;
+    
+    EK(int _n) : n(_n) {
+        adj = vector<vi>(n);
+        capacity = vector<vector<T>>(n, vector<T>(n));
+    }
 
-int bfs(int s, int t, vi &parent) {
-    fill(all(parent), -1);
-    parent[s] = -2;
-    queue<pii> q;
-    q.push({s, INF});
-
-    while (!q.empty()) {
-        int u, flow; 
-        tie(u, flow) = q.front(), q.pop();
-
-        for (int next : adj[cur]) {
-            if (parent[next] == -1 && capacity[cur][next]) {
-                parent[next] = cur;
-                int new_flow = min(flow, capacity[cur][next]);
-                if (next == t)
-                    return new_flow;
-                q.push({next, new_flow});
+    void addEdge(int u, int v, T c) {
+        adj[u].pb(v), adj[v].pb(u); 
+        capacity[u][v] = c;
+    }
+    
+    T bfs(int s, int t, vi &parent) {
+        fill(all(parent), -1);
+        parent[s] = s;
+        queue<pair<int, T>> q;
+        q.emplace(s, INF);
+        while (!q.empty()) {
+            int u = q.front().fst;
+            T flow = q.front().snd;
+            q.pop();
+            for (int v : adj[u]) {
+                if (parent[v] == -1 && capacity[u][v]) {
+                    parent[v] = u;
+                    T new_flow = min(flow, capacity[u][v]);
+                    if (v == t) return new_flow;
+                    q.emplace(v, new_flow);
+                }
             }
         }
+        return 0;
     }
-
-    return 0;
-}
-
-int maxflow(int s, int t) {
-    int flow = 0;
-    vi parent(n);
-    int new_flow;
-
-    while (new_flow = bfs(s, t, parent)) {
-        flow += new_flow;
-        int cur = t;
-        while (cur != s) {
-            int prev = parent[cur];
-            capacity[prev][cur] -= new_flow;
-            capacity[cur][prev] += new_flow;
-            cur = prev;
+    
+    T maxflow(int s, int t) {
+        T flow = 0, new_flow;
+        vi parent(n);
+        while ((new_flow = bfs(s, t, parent))) {
+            flow += new_flow;
+            int cur = t;
+            while (cur != s) {
+                int prev = parent[cur];
+                capacity[prev][cur] -= new_flow;
+                capacity[cur][prev] += new_flow;
+                cur = prev;
+            }
         }
+        return flow;
     }
-
-    return flow;
-}
+};
