79ff2fc1e450b5010d9a921747af62db2f19c391
NataTassi <natanieltassi@gmail.com>
2020-06-17 14:03:50 -0300

    add matching

---
+++ flow/matching.cpp
@@ -0,0 +1,46 @@
+struct matching {
+    // Indicate whether each node is on the left or call bipartition
+    int n;
+    vi match;
+    vector<vi> g;
+    vector<bool> vis, left;
+
+    void addEdge(int u, int v) { g[u].pb(v), g[v].pb(u); }
+
+    matching(int _n) { n = _n, match = vi(n, -1), g.resize(n), left.resize(n); } 
+
+    bool dfs(int u) {
+        if (vis[u]) return false;
+        vis[u] = true;
+        for (int v : g[u])
+            if (match[v] == -1 || dfs(match[v])) 
+                return match[v] = u, match[u] = v, true;
+        return false;
+    }
+
+    int max_matching() { // O(N * M)
+        int flow = 0;
+        forn(i, n) if (left[i])
+            vis.assign(n, 0), flow += dfs(i);
+        return flow;
+    }
+
+    bool bipartition() {
+        queue<int> q; 
+        vi dist(n, -1);
+        forn(i, n) if (dist[i] == -1) {
+            q.push(i), dist[i] = 0;
+            while (!q.empty()) {
+                int u = q.front(); q.pop();
+                if (dist[u] & 1) left[u] = 1; 
+                for (int v : g[u]) {
+                    if (dist[v] == -1)
+                        dist[v] = dist[u] + 1, q.push(v);
+                    else if ((dist[u] & 1) == (dist[v] & 1))
+                        return false; // graph isn't bipartite
+                }
+            }
+        }
+        return true;
+    }
+};
