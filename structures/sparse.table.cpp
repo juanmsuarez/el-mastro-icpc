3c398998c23660d5891ab26bb7e588830f4a4af2
NataTassi <natanieltassi@gmail.com>
2020-08-04 19:31:00 -0300

    update sparse table

--- structures/sparse.table.cpp
+++ structures/sparse.table.cpp
@@ -1,16 +1,17 @@
-// The operation has to be associative and idempotent
-#define lg(n) 31 - __builtin_clz(n)
-typedef int T; 
+#define lg(n) (31 - __builtin_clz(n))
+template<class T>
 struct RMQ {
-    const static int K = 10; // 2^K > N
-    T t[K][1 << K];
-    T& operator[](int p){ return t[0][p]; }
-    T get(int i, int j){ // O(1), [i, j)
+    int n; vector<vector<T>> t;
+    RMQ(int sz) { 
+        n = sz, t.assign(lg(n)+1, vector<T>(n)); 
+    }
+    T& operator[](int p) { return t[0][p]; }
+    T get(int i, int j) { // O(1), [i, j)
         int p = lg(j-i);
-        return min(t[p][i], t[p][j - (1 << p)]);
+        return max(t[p][i], t[p][j - (1 << p)]);
     }
-    void build(int n){ // O(n log n)
+    void build() { // O(n lg n)
         forn(p, lg(n)) forn(x, n - (1 << p))
-            t[p + 1][x] = min(t[p][x], t[p][x + (1 << p)]);
+            t[p + 1][x] = max(t[p][x], t[p][x + (1 << p)]);
     }
-} rmq;
+};
