b5fa7990611bd9e68344f1ec8e0c1f566ff3075c
NataTassi <natanieltassi@gmail.com>
2020-08-11 15:51:08 -0300

    update manacher

--- strings/manacher.cpp
+++ strings/manacher.cpp
@@ -1,19 +1,19 @@
-vi pal_array(string s)
-{
+void manacher(string s, vi &odd, vi &even) {
     int n = si(s);
     s = "@" + s + "$";
-
-    vi len(n + 1);
-    int l = 1, r = 1;
-
-    forsn(i, 1, n+1) {
-        len[i] = min(r - i, len[l + (r - i)]);
-
-        while (s[i - len[i]] == s[i + len[i]]) len[i]++;
-
-        if (i + len[i] > r) l = i - len[i], r = i + len[i];
+    odd = vi(n), even = vi(n);
+    int l = 0, r = -1;
+    forn(i, n) {
+        int k = i > r ? 1 : min(odd[l+r-i], r-i+1);
+        while (s[i+1-k] == s[i+1+k]) k++;
+        odd[i] = k--;
+        if (i+k > r) l = i-k, r = i+k;
     }
-
-    len.erase(begin(len));
-    return len;
+    l = 0, r = -1;
+    forn(i, n) {
+        int k = i > r ? 0 : min(even[l+r-i+1], r-i+1);
+        while (s[i-k] == s[i+1+k]) k++;
+        even[i] = k--;
+        if (i+k > r) l = i-k-1, r = i+k;
+    }
 }
