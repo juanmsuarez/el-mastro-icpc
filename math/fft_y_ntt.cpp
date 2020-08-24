c9e454d6a65e211b215241e78dafae441e66f526
Juaanma <juanmanuelsuarez10@gmail.com>
2020-08-24 17:08:01 -0300

    Update código FFT + NTT

--- math/fft_y_ntt.cpp
+++ math/fft_y_ntt.cpp
@@ -1,9 +1,8 @@
-// MODNTT-1 needs to be a multiple of MAXN !!
-// big mod and primitive root for NTT:
-// const ll MODNTT = 2305843009255636993;
-// const int RT = 5;
-// struct for FFT, for NTT is simple (ll with mod operations)
-struct CD {  // or typedef complex<double> CD; (but 4x slower)
+// N must be power of 2 !!
+using tf = ll;
+using poly = vector<tf>;
+// FFT
+struct CD {
 	double r,i;
 	CD(double r=0, double i=0):r(r),i(i){}
 	double real()const{return r;}
@@ -13,34 +12,49 @@ CD operator*(const CD& a, const CD& b){
 	return CD(a.r*b.r-a.i*b.i,a.r*b.i+a.i*b.r);}
 CD operator+(const CD& a, const CD& b){return CD(a.r+b.r,a.i+b.i);}
 CD operator-(const CD& a, const CD& b){return CD(a.r-b.r,a.i-b.i);}
-
-const double pi = acos(-1.0); // FFT
-CD cp1[MAXN+9],cp2[MAXN+9];  // MAXN must be power of 2 !!
-int R[MAXN+9];
-//CD root(int n, bool inv){ // NTT
-//	ll r=pot(RT,(MODNTT-1)/n); // pot: modular exponentiation
-//	return CD(inv?pot(r,MODNTT-2):r);
-//}
+const double pi=acos(-1.0);
+// NTT
+// M-1 needs to be a multiple of N !!
+// tf TIENE que ser ll
+// big mod and primitive root for NTT:
+/*
+const tf M=2305843009255636993,RT=5;
+struct CD {
+	tf x;
+	CD(tf x):x(x){}
+	CD(){}
+};
+CD operator*(const CD& a, const CD& b){return CD(mul(a.x,b.x));}
+CD operator+(const CD& a, const CD& b){return CD(add(a.x,b.x));}
+CD operator-(const CD& a, const CD& b){return CD(sub(a.x,b.x));}
+vector<tf> rts(N+9,-1);
+CD root(int n, bool inv){
+	tf r=rts[n]<0?rts[n]=pot(RT,(M-1)/n):rts[n];
+	return CD(inv?inv(r):r);
+}
+*/
+CD cp1[N+9],cp2[N+9];
+int R[N+9];
 void dft(CD* a, int n, bool inv){
 	forn(i,n)if(R[i]<i)swap(a[R[i]],a[i]);
-	for (int m=2;m<=n;m*=2){
-		double z = 2*pi/m*(inv?-1:1); // FFT
-		CD wi = CD(cos(z),sin(z)); // FFT
+	for(int m=2;m<=n;m*=2){
+		double z=2*pi/m*(inv?-1:1); // FFT
+		CD wi=CD(cos(z),sin(z)); // FFT
 		// CD wi=root(m,inv); // NTT
-		for (int j=0;j<n;j+=m){
+		for(int j=0;j<n;j+=m){
 			CD w(1);
 			for(int k=j,k2=j+m/2;k2<j+m;k++,k2++){
-				CD u=a[k]; CD v=a[k2]*w; a[k]=u+v; a[k2]=u-v; w=w*wi;
+				CD u=a[k];CD v=a[k2]*w;a[k]=u+v;a[k2]=u-v;w=w*wi;
 			}
 		}
 	}
-	if(inv) forn(i,n)a[i]/=n; // FFT
+	if(inv)forn(i,n)a[i]/=n; // FFT
 	//if(inv){ // NTT
-	//	CD z(pot(n,MODNTT-2)); // pot: modular exponentiation
+	//	CD z(inv(n));
 	//	forn(i,n)a[i]=a[i]*z;
 	//}
 }
-vi multiply(vi& p1, vi& p2){
+poly multiply(poly& p1, poly& p2){
 	int n=si(p1)+si(p2)+1;
 	int m=1,cnt=0;
 	while(m<=n)m+=m,cnt++;
@@ -51,8 +65,9 @@ vi multiply(vi& p1, vi& p2){
 	dft(cp1,m,false);dft(cp2,m,false);
 	forn(i,m)cp1[i]=cp1[i]*cp2[i];
 	dft(cp1,m,true);
-	vi res;
+	poly res;
 	n-=2;
-	forn(i,n)res.pb((ll)floor(cp1[i].real()+0.5)); // change for NTT
+	forn(i,n)res.pb((tf)floor(cp1[i].real()+0.5)); // FFT
+	//forn(i,n)res.pb(cp1[i].x); // NTT
 	return res;
 }
