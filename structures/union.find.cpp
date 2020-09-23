85d615a57da3a7b1af3cdd58b32204108d04cde5
NataTassi <natanieltassi@gmail.com>
2020-09-23 18:12:58 -0300

    update dsu

--- structures/union.find.cpp
+++ structures/union.find.cpp
@@ -4,7 +4,8 @@ struct DSU {
     int find(int u) { return par[u] == u ? u : par[u] = find(par[u]); }
     bool connected(int u, int v) { return find(u) == find(v); }
     bool join(int u, int v) {
-        if ((u = find(u)) == (v = find(v))) return false;
+        u = find(u), v = find(v);
+        if (u == v) return false;
         if (sz[u] < sz[v]) par[u] = v, sz[v] += sz[u];
         else par[v] = u, sz[u] += sz[v];
         return true;
