24738cc8c2e3084237b6ae58b90f64d963f354e1
NataTassi <natanieltassi@gmail.com>
2020-09-23 16:10:57 -0300

    update Mo's

--- others/mosalgorithm.cpp
+++ others/mosalgorithm.cpp
@@ -1,25 +1,23 @@
-const int Q = 2e5, SQ = 200;
-
-struct Query { // [l, r)
-    int l,r,id;
-    bool operator<(const Query &q){
-        if(l/SQ != q.l/SQ) return l < q.l;
-        return l/SQ & 1 ? r < q.r : r > q.r;
+struct Mo {
+    static const int SQ = 200;
+    struct Query { // [l, r)
+        int l, r, id;
+        bool operator<(const Query &q) {
+            if (l/SQ != q.l/SQ) return l < q.l;
+            return l/SQ & 1 ? r < q.r : r > q.r;
+        }
+    }; vector<Query> qs;
+    int res = 0, l = 0, r = 0, p = 0; vi ans;
+    Mo(int q) { ans = vi(q), qs.resize(q); }
+    void addQuery(int x, int y) { qs[p] = {x, y, p++}; }
+    void run() { // O((n + q) * sqrt(n) * (add() + del()))
+        sort(all(qs));
+        for (auto &q : qs) {
+            while (l > q.l) add(--l);
+            while (r < q.r) add(r++);
+            while (l < q.l) del(l++);
+            while (r > q.r) del(--r);
+            ans[q.id] = res;
+        }
     }
-} qs[Q];
-
-int ans[Q],res,pl,pr; // ans[i] = ans to ith query
-
-void mo(int m){ // O( (n+q) * sqrt(n) * (add() + remove()) )
-    forn(i,m) qs[i].id = i;
-    sort(qs, qs + m);
-    pl = 0, pr = 0, res = 0;
-    forn(i,m){
-        Query &q = qs[i];
-        while(pl > q.l) add(--pl);
-        while(pr < q.r) add(pr++);
-        while(pl < q.l) remove(pl++);
-        while(pr > q.r) remove(--pr);
-        ans[q.id] = res;
-    }
-}
+};
