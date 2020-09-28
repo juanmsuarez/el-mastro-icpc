f24487586fc5d181b212da3a037a82c9501b6d0a
NataTassi <natanieltassi@gmail.com>
2020-09-28 13:46:13 -0300

    update coordinate compression

--- others/coordinate_compression.cpp
+++ others/coordinate_compression.cpp
@@ -1,5 +1,4 @@
 void compress(vi &a){ // O(n lg n)
-    vi b(a); sort(all(b));
-    b.erase(unique(all(b)), b.end());
-    for(int &x:a) x = int(lower_bound(all(b),x) - b.begin());
+    vi b(a); sort(all(b)), b.erase(unique(all(b)), b.end());
+    for(int &x : a) x = int(lower_bound(all(b), x) - begin(b));
 }
