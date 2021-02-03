a85d24d11454b386b75f85865f08031d3569c915
Juaanma <juanmanuelsuarez10@gmail.com>
2021-02-03 17:19:42 -0300

    Update Mo's algorithm

--- misc/mosalgorithm.cpp
+++ misc/mosalgorithm.cpp
@@ -1,22 +1,61 @@
+// ONLY QUERIES
 struct Mo {
-    static const int SQ = 500;
+    static const int S = 500; // S = sqrt(N)
     struct Query { // [l, r)
         int l, r, id;
         bool operator<(const Query &q) {
-            if (l/SQ != q.l/SQ) return l < q.l;
-            return l/SQ & 1 ? r < q.r : r > q.r;
+            if (l/S != q.l/S) return l < q.l;
+            return l/S & 1 ? r < q.r : r > q.r;
         }
-    }; vector<Query> qs;
-    int l = 0, r = 0, p = 0, res = 0; vi ans;
+    }; 
+
+    vector<Query> qs;
+    int cl = 0, cr = 0, cq = 0, res = 0; vi ans;
+
     Mo(int q) : qs(q), ans(q) {}
-    void addQuery(int x, int y) { qs[p] = {x, y, p++}; }
+
+    void addQuery(int l, int r) { qs[cq] = {l, r, cq++}; }
     void run() { // O((n + q) * sqrt(n) * (add() + del()))
         sort(all(qs));
         for (auto &q : qs) {
-            while (l > q.l) add(--l);
-            while (r < q.r) add(r++);
-            while (l < q.l) del(l++);
-            while (r > q.r) del(--r);
+            while (cl > q.l) add(--cl);
+            while (cr < q.r) add(cr++);
+            while (cl < q.l) del(cl++);
+            while (cr > q.r) del(--cr);
+            ans[q.id] = res;
+        }
+    }
+};
+
+// QUERIES AND UPDATES
+struct Mo {
+    static const int S = 2700; // S = cbrt(2 * N * N)
+    struct Query { // [l, r)
+        int l, r, id, t;
+        bool operator<(const Query &o) {
+            if (l/S != o.l/S) return l < o.l;
+            if (r/S != o.r/S) return r < o.r;
+            return t < o.t;
+        }
+    }; 
+    struct Update { int x, v; }; 
+
+    vector<Query> qs; vector<Update> us;
+    int cl = 0, cr = 0, ct = 0, res = 0; vi ans;
+
+    void addQuery(int l, int r) { qs.pb(l, r, si(qs), si(us)); }
+    void addUpdate(int x, int v) { us.pb(x, v); }
+    // O(q*s * (add() + del()) + n * del() + n^2/s * add() + q*(n/s)^2 * (upd() + undo()))
+    void run() { // si n = q y ops. ctes. O(n^(5/3))
+        ans.resize(si(qs));
+        sort(all(qs));
+        for (auto &q : qs) {
+            while (cl > q.l) add(--cl);
+            while (cr < q.r) add(cr++);
+            while (cl < q.l) del(cl++);
+            while (cr > q.r) del(--cr);
+            while (ct < q.t) upd(ct++);
+            while (ct > q.t) undo(--ct);
             ans[q.id] = res;
         }
     }
