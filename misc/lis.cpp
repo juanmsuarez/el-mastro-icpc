9231efc71c9c8884f3e2241c9e06b4af79daf960
NataTassi <natanieltassi@gmail.com>
2021-03-05 12:18:08 -0300

    update lis

--- misc/lis.cpp
+++ misc/lis.cpp
@@ -1,28 +1,23 @@
+const int INF = 2e9;
 int lis(const vi &a) { // O(n lg n)
-    int n = si(a), INF = 2e9, r = 0;
-    vi v(n+1,INF); v[0] = -INF;
-    forn(i,n){
-        int j = int(upper_bound(all(v), a[i]) - v.begin());
-        if(v[j-1] < a[i] && a[i] < v[j]) v[j] = a[i], r = max(r,j);
+    int r = 0;
+    vi v(si(a) + 1, INF); v[0] = -INF;
+    for (int x : a) {
+        int j = int(upper_bound(all(v), x) - begin(v));
+        if (v[j-1] < x) v[j] = x, r = max(r, j);
     }
     return r;
 }
-
 --------------------------------------------------------------------
-
-vi path;
-int lis(const vi &a) { // O(n lg n)
-    int n = si(a), INF = 2e9, r = 0;
-    vi v(n+1,INF),id(n+1),p(n); 
-    v[0] = -INF;
-
-    forn(i,n){
-        int j = int(upper_bound(all(v), a[i]) - v.begin());
-        if(v[j-1] < a[i] && a[i] < v[j]) 
-            v[j] = a[i], r = max(r,j), id[j] = i, p[i] = id[j-1];
+const int INF = 2e9;
+vi lis(const vi &a) { // O(n lg n)
+    int n = si(a), r = 0;
+    vi v(n+1, INF), id(n+1), p(n); v[0] = -INF;
+    forn(i, n) {
+        int x = a[i], j = int(upper_bound(all(v), x) - begin(v));
+        if (v[j-1] < x) v[j] = x, r = max(r, j), id[j] = i, p[i] = id[j-1];
     }
-
-    path = vi(r); int c = id[r];
-    forn(i,r) path[r-i-1] = a[c], c = p[c];
-    return r;
+    vi path(r); int c = id[r];
+    dforn(i, r) path[i] = a[c], c = p[c];
+    return path;
 }
