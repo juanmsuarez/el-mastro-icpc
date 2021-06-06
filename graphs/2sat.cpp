eff8b13685a277763f8ae36f44dde2d7c89099aa
NataTassi <natanieltassi@gmail.com>
2021-06-06 20:10:26 -0300

    update 2sat

--- graphs/2sat.cpp
+++ graphs/2sat.cpp
@@ -2,19 +2,19 @@
 // Every edge represents an implication, to add a clause (a or b), use add_or(a, b)
 // val[comp[i]] = value of variable i
 struct SAT {
+    int n, id, comps, x;
+    vi low, idx, comp;
+    vector<bool> val;
     vector<vi> g;
     stack<int> q;
-    vector<bool> val;
-    vi low, idx, comp;
-    int n, id, comps, x;
 
-    SAT(int vars) {
-        n = vars, g.resize(2*n), id = 0, comps = 0;
-        low = vi(2*n), idx = vi(2*n, -1), comp = vi(2*n, -1);
-    }
-
+    SAT(int vars) : n(vars), low(2*n), val(2*n), g(2*n) { reset(); }
+    void reset() { id = comps = 0, idx.assign(2*n, -1), comp.assign(2*n, -1); }
     int neg(int u) { return u >= n ? u-n : u+n; }
+
     void add_or(int a, int b) { g[neg(a)].pb(b), g[neg(b)].pb(a); }
+    void add_implication(int a, int b) { g[a].pb(b), g[neg(b)].pb(neg(a)); }
+    void fix_variable(int var) { g[neg(var)].pb(var); }
 
     void tarjan(int u) {
         low[u] = idx[u] = id++;
@@ -27,11 +27,12 @@ struct SAT {
         }
         if (low[u] == idx[u]) {
             do { x = q.top(), q.pop(), comp[x] = comps; } while (x != u);
-            val.pb(comp[neg(u)] < 0), comps++;
+            val[comps++] = comp[neg(u)] < 0;
         }
     }
 
     bool satisfiable() {
+        reset();
         forn(i, 2*n) if (idx[i] == -1) tarjan(i);
         forn(i, n) if (comp[i] == comp[neg(i)]) return false;
         return true;
